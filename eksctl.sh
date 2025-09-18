962948f17dbee6ef798135051181eaf329170396
Ved Badadhe <vbbadadhe@gmail.com>
2025-09-19 00:59:09 +0530

    Added The Manifest For the k8s

---
+++ eksctl.sh
@@ -0,0 +1,6 @@
+# To setup teh eksctl in teh AWS CloudShell
+curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin 
+
+eksctl version
